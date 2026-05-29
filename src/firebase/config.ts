c59f30c901db0be12b0470eb2079befed3e66f6a
mohammedabdelnasser884-hash <mohammedabdelnasser884@gmail.com>
2026-05-29 18:34:41 +0300

    Add src/firebase/config.ts

---
+++ src/firebase/config.ts
@@ -0,0 +1,9 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSy...",
+  authDomain: "sanad-legal-pro.firebaseapp.com",
+  projectId: "sanad-legal-pro",
+  storageBucket: "sanad-legal-pro.firebasestorage.app",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef"
+};
