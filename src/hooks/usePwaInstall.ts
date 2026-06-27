0cbc74e565c393120e1b39f1c4aefdac2131082b
mohammedabdelnasser884-hash <mohammedabdelnasser884@gmail.com>
2026-06-27 03:39:29 +0300

    Add src/hooks/usePwaInstall.ts

--- src/hooks/usePwaInstall.ts
+++ src/hooks/usePwaInstall.ts
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { toast } from '../utils';
 
 export function usePwaInstall() {
     const [pwaInstallable, setPwaInstallable] = useState(false);
